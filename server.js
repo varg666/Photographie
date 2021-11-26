8a2dd91ddb7454d68a9ebaecc9a7f15e3fb26f3c
varg666 <a.hegazy.dev@gmail.com>
2021-11-26 08:48:38 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = rquire('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.send('hello!!')
+})
+
+
+
+app.listen(PORT, ()=>{
+
+console.log(`server running on port ${PORT}`)
+
+})
